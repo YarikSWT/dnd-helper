985bae0d21cf23add46e675c3436dc515f7d88a1
Yaroslav Yudinskikh <yaskih@mail.ru>
2020-10-28 11:56:51 +0300

    some printing

--- preview.py
+++ preview.py
@@ -33,7 +33,7 @@ def get_image_names(url):
 
     found = page_soup.findAll('script')
 
-    script = found[28]
+    script = found[29]
     text = str(script)
     text = text.replace('window.cloudSettings =', '')
     text = text.replace('<script>', '')
@@ -43,9 +43,13 @@ def get_image_names(url):
     text = text.replace('\\x', '')
     text = text.replace('\\x', '')
 
+    print('start parse to json')
+
     jsn = json.loads(text)
 
     imgs_names = jsn['folders']['tree'][1]['list'][0]['items']
+
+    print('imgs_names: ', imgs_names)
 
     # print(imgs_names)
 
@@ -54,7 +58,9 @@ def get_image_names(url):
 def url_to_image(url):
   # download the image, convert it to a NumPy array, and then read
 	# it into OpenCV format
+  print('trying to load image from url ', url)
   resp = urllib.request.urlopen(url)
+  print('done load')
   image = np.asarray(bytearray(resp.read()), dtype="uint8")
   image_cvt = cv2.imdecode(image, cv2.IMREAD_COLOR)
   image = cv2.cvtColor(image_cvt, cv2.COLOR_BGR2RGB)
@@ -67,6 +73,8 @@ def get_images_by_names(names):
         splited = name.split('Исходники')
         norma = splited[0] + quote('Исходники') + splited[1]
         normalized_names.append(norma)
+
+    print('normalized: ', len(normalized_names))
 
     imgs = [url_to_image(IMG_BASE_URL + img_name) for img_name in normalized_names]
 
